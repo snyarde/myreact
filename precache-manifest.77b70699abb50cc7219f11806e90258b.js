249dcdeb5ffd730effee5b62a7043e18bb8473c7
Timothy McHugh <Timothy.McHugh@yarde.com>
2019-12-06 10:32:58 -0500

    Updates

---
+++ precache-manifest.77b70699abb50cc7219f11806e90258b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3a14325a10b1d7247604f63e0bdaa3",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "3cb20364a4524e9b598d",
+    "url": "/myreact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "393a1977700cdaa3abb4",
+    "url": "/myreact/static/js/2.3e774d44.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myreact/static/js/2.3e774d44.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cb20364a4524e9b598d",
+    "url": "/myreact/static/js/main.b42d0f25.chunk.js"
+  },
+  {
+    "revision": "8a2b3f9e0320f142cb81",
+    "url": "/myreact/static/js/runtime-main.c0bf9741.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
